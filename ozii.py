7b550856794da2983c0230ad2bd4fa53d50975cb
juangallostra <juangallostra@gmail.com>
2017-10-07 21:50:30 +0200

    Command line support and code refactor

--- ozii.py
+++ ozii.py
@@ -2,43 +2,46 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import defaultdict
+import argparse
 #from scipy.sparse import csr_matrix
 #from PIL import Image
 
-alphabet = defaultdict(lambda: 0, {
-    'e': 10.0,
-    't': 9.62,
-    'a': 9.23,
-    'o': 8.85,
-    'i': 8.46,
-    'n': 8.08,
-    's': 7.69,
-    'r': 7.31,
-    'h': 6.92,
-    'd': 6.54,
-    'l': 6.15,
-    'u': 5.77,
-    'c': 5.34,
-    'm': 5.00,
-    'f': 4.62,
-    'y': 4.23,
-    'w': 3.85,
-    'g': 3.46,
-    'p': 3.08,
-    'b': 2.69,
-    'v': 2.31,
-    'k': 1.92,
-    'x': 1.54,
-    'q': 1.15,
-    'j': 0.77,
-    'z': 0.34,
-    '.': 4.9e-7,
-    '?': 5.1e-7,
-    ' ': 0
-})
+def build_alphabet():
+    alphabet = defaultdict(lambda: 0, {
+        'e': 10.0,
+        't': 9.62,
+        'a': 9.23,
+        'o': 8.85,
+        'i': 8.46,
+        'n': 8.08,
+        's': 7.69,
+        'r': 7.31,
+        'h': 6.92,
+        'd': 6.54,
+        'l': 6.15,
+        'u': 5.77,
+        'c': 5.34,
+        'm': 5.00,
+        'f': 4.62,
+        'y': 4.23,
+        'w': 3.85,
+        'g': 3.46,
+        'p': 3.08,
+        'b': 2.69,
+        'v': 2.31,
+        'k': 1.92,
+        'x': 1.54,
+        'q': 1.15,
+        'j': 0.77,
+        'z': 0.34,
+        '.': 4.9e-7,
+        '?': 5.1e-7,
+        ' ': 0
+    })
 
-for letter in [key for key in alphabet.keys() if key.isalpha()]:
-    alphabet[letter.upper()] = alphabet[letter]+(1e-7)
+    for letter in [key for key in alphabet.keys() if key.isalpha()]:
+        alphabet[letter.upper()] = alphabet[letter]+(1e-7)
+    return alphabet
 
 def cos(x):
     return np.cos(180 * x / np.pi)
@@ -52,10 +55,7 @@ def inverse(x):
 def transformer(x):
     return inverse(sin(cos(x)))
 
-x = np.linspace(0, 1, 1001)
-x = x[1:]
-
-def transform(text):
+def transform(x, alphabet, text):
     n = len(text)
     y = 0
     for i in range(len(text)):
@@ -65,17 +65,20 @@ def transform(text):
     y = (0.5/max_y) * y
     return y
 
-def sentence_transformer(sentence):
+def sentence_transformer(x, alphabet, sentence):
     words = sentence.split()
     y = np.zeros(x.shape)
     for i, word in enumerate(words):
-        y += transform(word)
+        y += transform(x, alphabet, word)
     max_y = np.max(np.abs(y))
     y = (0.5/max_y) * y
     return y
 
 def generate_image(sentence, pixels=500, dir="output"):
-    y = sentence_transformer(sentence)
+    alphabet = build_alphabet()
+    x = np.linspace(0, 1, 1001)
+    x = x[1:]
+    y = sentence_transformer(x, alphabet, sentence)
     size = pixels / 10
     fig = plt.figure(figsize=(10, 10))
     plt.plot(x, y, linewidth=1, c='k')
@@ -98,3 +101,24 @@ def generate_image(sentence, pixels=500, dir="output"):
         return arr
     csr = csr_matrix(arr)
     return csr'''
+
+def main():
+    if args.size is None:
+        parser.error('Invalid size')
+    elif args.dir is None:
+        parser.error('Invalid output directory')
+    generate_image(args.text, args.size, args.dir)
+    # command line parsing
+
+
+
+if __name__ == '__main__':
+    # Command line argument parsing
+    parser = argparse.ArgumentParser(description='Generate PulseGraphs from Text data')
+
+    parser.add_argument('-t','--text', help='Input sentence', required=True)
+    parser.add_argument('-s','--size', help='Output image size in pixels', type=int, nargs='?', default=500)
+    parser.add_argument('-d','--dir', help='Output directory', nargs='?', default='output')
+
+    args = parser.parse_args()
+    main()
